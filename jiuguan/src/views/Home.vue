020e36ec7160d6d09420f95e03b8c250ceb10544
Sahura <1747889818@qq.com>
2020-12-07 11:45:43 +0800

    ✨添加vuetify

--- jiuguan/src/views/Home.vue
+++ jiuguan/src/views/Home.vue
@@ -2,6 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-card
+    color="#9999">
+      test
+    </v-card>
   </div>
 </template>
 
