c32b7c6d59f25372162fd479ff1cf37be1352d76
chuan <912297261@qq.com>
2020-12-08 16:51:13 +0800

    添加了开始页的样式

---
+++ jiuguan/src/views/Start.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="start">
+      <div class="background">
+          <img :src="startSrc" width="100%" height="100%" alt="" />
+      </div>
+      <div class="button">
+          <a href="/login">
+             <v-btn depressed small color="primary">Primary</v-btn>
+          </a>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            startSrc : require("/Users/sairen/vuelearn/JiuGuan/jiuguan/src/images/startImg.png")
+        }
+    }
+}
+</script>
+
+<style>
+.button {
+    width: 30px;
+    height: 30px;
+}
+
+.background {
+    background-size: 100% 100%;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    background-position: center;
+    position: fixed;
+    z-index: -1;
+}
+</style>
