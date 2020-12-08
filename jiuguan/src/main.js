d624829c3615a1d554aa98348db639f794aaf942
chuan <912297261@qq.com>
2020-12-08 20:47:58 +0800

    :heavy_plus_sign: 引入组件以顺利展示icon

--- jiuguan/src/main.js
+++ jiuguan/src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
--- jiuguan/src/views/Login.vue
+++ jiuguan/src/views/Login.vue
@@ -1,13 +1,15 @@
 <template>
-  <h1>这是登陆界面</h1>
+  <div id="inputLogin">
+    <h1 class="title">进入酒馆</h1>
+    <v-input :messages="['Messages']" append-icon="clear">
+      <v-text-field>
+      </v-text-field>
+    </v-input>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
