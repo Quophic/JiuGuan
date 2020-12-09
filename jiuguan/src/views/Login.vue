29f51154b8fbce7b3d606e8027b9760892d5cd64
chuan <912297261@qq.com>
2020-12-09 11:00:51 +0800

    :sparkles: 增加了登录成功页面 优化登录页面的跳转逻辑

--- jiuguan/src/views/Login.vue
+++ jiuguan/src/views/Login.vue
@@ -4,9 +4,19 @@
       
     </div>
     <h1 class="title">进入酒馆</h1>
-    <div class="loginInput">
+    <div class="usernameInput">
       <v-text-field outlined clearable prepend-inner-icon="perm_identity" label="login"></v-text-field>
+    </div>
+    <div class="passwordInput">
       <v-text-field outlined clearable type="password" prepend-inner-icon="lock" label="password"></v-text-field>
+    </div>
+    <div class="logbutton">
+      <a href="/loginWelcome">
+        <v-btn id="loginBtn" depressed large height="28px" width="95px">登录</v-btn>
+      </a>
+      <a href="/register">
+        <v-btn id="regBtn" depressed large height="28px" width="95px">注册</v-btn>
+      </a>  
     </div>
   </div>
 </template>
@@ -16,18 +26,45 @@ export default {};
 </script>
 
 <style>
-.loginInput {
+.usernameInput,
+.passwordInput {
   position: fixed;
-  top:100px;
-  left: 100px;
   width: 182px;
   height: 20px;
+}
+
+.usernameInput {
+  top: 100px;
+  left: 100px;
+}
+
+.passwordInput {
+  top: 180px;
+  left: 100px;
 }
 
 h1 {
   position: fixed;
   top: 60px;
   left: 150px;
+}
+
+.logbutton {
+  position: fixed;
+  top: 300px;
+  left: 100px;
+}
+
+#loginBtn {
+  position: fixed;
+  top: 300px;
+  left: 75px;
+}
+
+#regBtn {
+  position: fixed;
+  top: 300px;
+  left: 200px;
 }
 
 </style>
---
+++ jiuguan/src/views/LoginWelcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>这是登陆成功的欢迎页面</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
