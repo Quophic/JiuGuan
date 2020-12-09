1aee609c9578f06c4ad26d158a06f6633ecc325e
chuan <912297261@qq.com>
2020-12-09 23:57:08 +0800

    :sparkles: 将token存储到localStorage和vuex中

--- jiuguan/src/store/index.js
+++ jiuguan/src/store/index.js
@@ -5,13 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state:{
+    //存储token
+    Authorization: localStorage.getItem('Authorization') ? localStorage.getItem('Authorization') : ''
 
   },
   actions:{
 
   },
   mutations:{
-
+    //修改tokne，并将token存入localStorage
+    changeLogin (state, user) {
+        state.Authorization = user.Authorization;
+        localStorage.setItem('Authorization', user.Authorization);
+    }
   },
   modules:{
 
