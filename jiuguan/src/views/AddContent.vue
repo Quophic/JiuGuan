61e2322f0c1a342cacaff086bea0ecf5c423cca9
Sahura <1747889818@qq.com>
2020-12-10 19:31:55 +0800

    完善content页面

---
+++ jiuguan/src/views/AddContent.vue
@@ -0,0 +1,110 @@
+<template>
+    <div>
+       <h1>这是添加content的页面</h1> 
+       <div class="justify">
+            <v-btn
+                text
+                height="70px"
+                width="70px"
+                class="rounded-circle add"
+                @click="sendContent"
+                >
+                
+                    <v-icon
+                    color="blue"
+                    size="50px"
+                    >mdi-telegram</v-icon>
+                </v-btn>
+           <div class="input" >
+              
+                <v-textarea
+                v-model="newContent"
+                autofocus
+                auto-grow
+                background-color="white"
+                outlined
+                counter="200"
+                placeholder="说点什么？"
+                >
+                </v-textarea>
+                {{newContent}}
+           </div>   
+           </div>
+             <div class="topicPlace">
+            </div>
+           
+       
+       <v-app-bar
+       absolute
+       bottom
+        right
+       :class="'rounded-t-xl'"
+      
+       >
+        <v-btn
+        text
+         class="bottomBTN1">
+         <v-icon>mdi-image-multiple-outline</v-icon>
+        </v-btn>
+        <v-btn
+        text
+         class="bottomBTN2">
+         <v-icon size="27px">mdi-pencil-box-multiple-outline</v-icon>
+        </v-btn>
+       </v-app-bar>
+       
+    </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            newContent:"",
+        }
+    },
+    methods:{
+        sendContent(){
+            console.log(this.newContent)
+            this.newContent = ""
+        }
+    }
+}
+</script>
+
+<style  scoped>
+.justify{
+    position: relative;
+    top:50px;
+}
+.input{
+    position:absolute;
+    background-color:white;
+    margin:auto;
+    top:100px;
+    left:10%;
+    width:80%;
+}
+.add{
+    position: absolute;
+    right:10%;
+    
+}
+.bottomBTN1{
+    position: absolute;
+    left:25%;
+}
+.bottomBTN2{
+    position: absolute;
+    right:25%;
+}
+.topicPlace{
+    position:absolute;
+    bottom:50%;
+    left:15%;
+    width:70%;
+    
+}
+
+</style>
