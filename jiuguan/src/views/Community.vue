274aea8998dd6351f55e54743ba79bd4fad28e8a
Sahura <1747889818@qq.com>
2020-12-11 10:16:21 +0800

    代码重构，无改动

--- jiuguan/src/views/Community.vue
+++ jiuguan/src/views/Community.vue
@@ -1,117 +1,98 @@
 <template>
   <div>
-    
-    <div style=" margin:30px 10px;">
+    <div style="margin: 30px 10px">
       <v-card
-      style="margin:20px 0;"
-      v-for="(item, index) in contents"
-      :key="index"
-    >
-     <v-card-title >
-      <v-avatar>
-        <v-img max-height="100%" src="../assets/logo.png"></v-img>
-      </v-avatar>
-       <span class="id">{{item.FromName}}</span>
-     </v-card-title>
-     <v-card-text>
-       {{item.Content}}
-     </v-card-text>
-
-     <v-card-actions>
-      <v-row
-        justify="end"
+        style="margin: 20px 0"
+        v-for="(item, index) in contents"
+        :key="index"
       >
-        <v-btn
-        text
-        height="30px"
-        width="80px"
-        elevation="0"
-       >
-         <v-icon color="grey" >mdi-thumb-up</v-icon>
-         <!-- 点赞数为零时不显示 -->
-        <span v-show="item.LikeNum">{{item.LikeNum}}</span> 
-       </v-btn>
-       <v-btn
-        text
-        height="30px"
-        width="80px"
-        elevation="0"
-       >
-         <v-icon color="grey" >mdi-message-processing-outline</v-icon>
-        <span class="num" v-show="item.ReplyNum">{{item.ReplyNum}}</span> 
-       </v-btn>
-      </v-row>
-     </v-card-actions>
-      
-    </v-card>
-    
-       <v-btn  text @click="test">点击刷新</v-btn>
+        <v-card-title>
+          <v-avatar>
+            <v-img max-height="100%" src="../assets/logo.png"></v-img>
+          </v-avatar>
+          <span class="id">{{ item.FromName }}</span>
+        </v-card-title>
+        <v-card-text>
+          {{ item.Content }}
+        </v-card-text>
+
+        <v-card-actions>
+          <v-row justify="end">
+            <v-btn text height="30px" width="80px" elevation="0">
+              <v-icon color="grey">mdi-thumb-up</v-icon>
+              <!-- 点赞数为零时不显示 -->
+              <span v-show="item.LikeNum">{{ item.LikeNum }}</span>
+            </v-btn>
+            <v-btn text height="30px" width="80px" elevation="0">
+              <v-icon color="grey">mdi-message-processing-outline</v-icon>
+              <span class="num" v-show="item.ReplyNum">{{
+                item.ReplyNum
+              }}</span>
+            </v-btn>
+          </v-row>
+        </v-card-actions>
+      </v-card>
+
+      <v-btn text @click="test">点击刷新</v-btn>
     </div>
     <a href="/community/addContent">
-    <v-btn
-      color="red"
-      height="70px"
-      width="70px"
-      :elevation="3" 
-      size="70px" 
-      class="addNewContent rounded-circle" 
+      <v-btn
+        color="red"
+        height="70px"
+        width="70px"
+        :elevation="3"
+        size="70px"
+        class="addNewContent rounded-circle"
       >
-      <v-icon
-      color="white" 
-      size="50px"
-      >mdi-plus</v-icon>
+        <v-icon color="white" size="50px">mdi-plus</v-icon>
       </v-btn>
     </a>
-    
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-      return{
-        contents:[],
-        pageNum:1
-      }
+  data() {
+    return {
+      contents: [],
+      pageNum: 1,
+    };
+  },
+  methods: {
+    //  请求comment测试
+    test() {
+      //当contents被清空时，pageNum要赋值为1
+      this.$axios
+        .get("/getcommentbytime?PageSize=10&PageNum=" + this.pageNum++)
+        .then((response) => {
+          if (response.data) {
+            console.log(response.data.date);
+            this.contents.push(...response.data.date);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    methods:{
-      //  请求comment测试
-        test(){
-          //当contents被清空时，pageNum要赋值为1
-            this.$axios.get('/getcommentbytime?PageSize=10&PageNum='+this.pageNum++).then(response => {
-                    if (response.data) {
-                        console.log(response.data.date)
-                        this.contents.push(...response.data.date)
-                    }
-                }).catch(err => {
-                    console.log(err)
-                })
-
-        },
-    }
+  },
 };
 </script>
 
 <style  scoped>
-
-.contentPlace{
-  margin:30px 10px;
+.contentPlace {
+  margin: 30px 10px;
 }
-.id{
-  margin-left:30px;
-}
-
-
-.num{
-  font-size:10px
-}
-.addNewContent{
-  background-color:rgb(247, 247, 247);
-  position:fixed;
-  bottom:30px;
-  right:20px;
+.id {
+  margin-left: 30px;
 }
 
-
-
+.num {
+  font-size: 10px;
+}
+.addNewContent {
+  background-color: rgb(247, 247, 247);
+  position: fixed;
+  bottom: 30px;
+  right: 20px;
+}
 </style>
