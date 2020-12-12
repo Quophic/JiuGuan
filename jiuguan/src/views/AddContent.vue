7a9ad476388dd6591311e28e067ca23b345b9a55
chuan <912297261@qq.com>
2020-12-12 14:57:47 +0800

    :sparkles: 优化了发帖的用户体验

--- jiuguan/src/views/AddContent.vue
+++ jiuguan/src/views/AddContent.vue
@@ -67,6 +67,7 @@ export default {
     sendContent() {
       if (this.newContent.match(/[\S]/) == null  ) {
         this.placeHolder = "禁止输入为空";
+        alert("输入框不得为空");
       } else if(this.newContent.length <= 200) {
         console.log(this.newContent);
         this.$axios
@@ -81,7 +82,8 @@ export default {
           .then((response) => {
             console.log(response);
             console.log(response.data);
-            alert(response.data.msg);
+            // alert(response.data.msg);
+            this.$router.push("/community");
           });
 
         this.newContent = ""; 
