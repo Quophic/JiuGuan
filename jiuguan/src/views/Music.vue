30376f49a87ceb765877f5246df710d3c511ee5c
Sahura <1747889818@qq.com>
2020-12-12 11:02:16 +0800

    添加返回主页面按钮

--- jiuguan/src/views/Music.vue
+++ jiuguan/src/views/Music.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="background">
-    <div><v-btn></v-btn></div>
+    <div class="quit">
+      <a href="/home">
+        <v-btn icon color="#E16C6C" class="quitIcon">
+          <v-icon large>keyboard_arrow_left</v-icon>
+        </v-btn>
+      </a>
+    </div>
+
     <div class="p1" @click="right"><v-img :src="music[0].src"></v-img></div>
     <div class="p2"><v-img :src="music[1].src"></v-img></div>
     <div class="p3" @click="left"><v-img :src="music[2].src"></v-img></div>
     <!-- 图片大小不一样，看起来额 -->
-    <div class="name">{{music[1].name}}</div>
+    <div class="name">{{ music[1].name }}</div>
   </div>
 </template>
 
@@ -16,7 +23,7 @@ export default {
       music: [
         { src: require("../images/photo1.png"), name: "海浪" },
         { src: require("../images/photo2.png"), name: "舒缓" },
-        { src: require("../images/photo.png"), name:"雨声" },
+        { src: require("../images/photo.png"), name: "雨声" },
       ],
       a: 0,
     };
@@ -63,15 +70,23 @@ export default {
   width: 80px;
   top: 180px;
 }
-.name{
+.name {
   font-family: PingFang SC;
   position: absolute;
-  top:270px;
-  width:20%;
-  font-size:20px;
+  top: 270px;
+  width: 20%;
+  font-size: 20px;
   text-align: center;
-  color:#B97C4F;
-  left:40%;
+  color: #b97c4f;
+  left: 40%;
+}
+a {
+  text-decoration: none;
+}
+.quit{
+  position: absolute;
+  top:30px;
+  left:20px
 }
 </style>
 
