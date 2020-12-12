1cb2407dcc032fb03f7c371d585a24b2c6deae45
Sahura <1747889818@qq.com>
2020-12-12 14:01:50 +0800

    添加评论回复界面

---
+++ jiuguan/src/views/Reply.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        sss
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            replies:[
+
+            ]
+        }
+    },
+    create(){
+        getReply()
+    },
+    methods:{
+          getReply(){
+      this.$axios.get("/getreply?CommentId="+sessionStorage.getItem("commentId")+"&PageSize=100"+"&PageNum=1")
+      .then((response)=>{
+        if(response.data){
+          console.log(response.data)
+        }
+      }) .catch((err) => {
+          console.log(err);
+        });
+    }
+    }
+}
+</script>
