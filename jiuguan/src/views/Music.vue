4da2a99856d360127cfd317ca041663d7d32018a
Sahura <1747889818@qq.com>
2020-12-12 10:57:16 +0800

    图片点击切换

--- jiuguan/src/views/Music.vue
+++ jiuguan/src/views/Music.vue
@@ -1,16 +1,77 @@
 <template>
   <div class="background">
-      
+    <div><v-btn></v-btn></div>
+    <div class="p1" @click="right"><v-img :src="music[0].src"></v-img></div>
+    <div class="p2"><v-img :src="music[1].src"></v-img></div>
+    <div class="p3" @click="left"><v-img :src="music[2].src"></v-img></div>
+    <!-- 图片大小不一样，看起来额 -->
+    <div class="name">{{music[1].name}}</div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      music: [
+        { src: require("../images/photo1.png"), name: "海浪" },
+        { src: require("../images/photo2.png"), name: "舒缓" },
+        { src: require("../images/photo.png"), name:"雨声" },
+      ],
+      a: 0,
+    };
+  },
+  methods: {
+    left() {
+      this.music.push(this.music.shift());
+    },
+    right() {
+      this.music.unshift(this.music.pop());
+    },
+  },
+};
 </script>
 
 <style  scoped>
+.background {
+  background-color: #fcf0d5;
+  height: 100%;
+}
+.pictures {
+  position: absolute;
+  width: 40000px;
+  margin: 0 auto;
+  top: 120px;
+  width: 100px;
+}
+.p1 {
+  position: absolute;
+  left: 5%;
+  top: 180px;
+  width: 80px;
+}
 
+.p2 {
+  position: absolute;
+  left: 35%;
+  width: 120px;
+  top: 150px;
+}
+.p3 {
+  position: absolute;
+  right: 5%;
+  width: 80px;
+  top: 180px;
+}
+.name{
+  font-family: PingFang SC;
+  position: absolute;
+  top:270px;
+  width:20%;
+  font-size:20px;
+  text-align: center;
+  color:#B97C4F;
+  left:40%;
+}
 </style>
 
